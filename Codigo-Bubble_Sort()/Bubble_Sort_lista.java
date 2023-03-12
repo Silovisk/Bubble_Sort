27cda0b55ae737b86548b80fc3df0d77d227279c
Silasaraujo <77333741+Silovisk@users.noreply.github.com>
2023-03-12 15:34:14 -0300

    Initial commit

---
+++ Codigo-Bubble_Sort()/Bubble_Sort_lista.java
@@ -0,0 +1,24 @@
+public void ordena_bubble()
+        {
+        Msg.msg_central("Bubble Sort.");
+
+        boolean trocou; // pra ver se houve troca
+        do {
+        trocou = false; // inicializa sem troca
+
+        NohNumeros atual = this.primeiro; // a variavel auxiliar começa pelo primeiro nó da lista
+
+        while (atual != null && atual.getProximo() != null) // loop enquanto nao chegar no final
+        {
+        if (atual.getNumero() > atual.getProximo().getNumero()) // ve se os elementos estao fora de ordem
+        {
+        int aux = atual.getNumero(); // guarda o valor atual
+        atual.setNumero(atual.getProximo().getNumero()); // o  atual recebe o valor do proximo
+        atual.getProximo().setNumero(aux); // o proximo recebe o valor guardado
+        trocou = true;
+        }
+        atual = atual.getProximo(); // passa pro proximo da lista
+        }
+        } while (trocou);
+
+        }
---
+++ Codigo-Bubble_Sort()/Bubble_Sort_vetor.java
@@ -0,0 +1,23 @@
+    public void ordena_BubbleSort() {
+      MsgVetor.msg_central("BubbleSort");
+
+      int tamanho_vetor = this.getTamanho();
+
+      boolean trocou = true;
+
+      while (trocou) {
+        trocou = false;
+        for (int i = 1; i < tamanho_vetor; i++) // percorre vetor
+        {
+          if (this.vetor[i - 1] > this.vetor[i]) // se o atual for maior que proximo
+          {
+            int aux = this.vetor[i]; // guarda o valor atual
+            this.vetor[i] = this.vetor[i - 1]; // move o proximo para o atual
+            this.vetor[i - 1] = aux; // o anterior recebe o valor guardado
+
+            trocou = true;
+          }
+        }
+        tamanho_vetor--; //reduz tamanho do vetor
+      }
+    }
